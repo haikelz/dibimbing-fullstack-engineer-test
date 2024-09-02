43570eb57014e68347f89f9cab72f3ff14060692
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-09-02 22:53:41 +0700

    fix: deployment

---
+++ prisma/migrations/20240902152658_migration/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Notes" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Notes_pkey" PRIMARY KEY ("id")
+);
